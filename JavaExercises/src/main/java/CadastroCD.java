6caeb64654f2ba1d4f34957b5fbdc52d524b6875
Chagas <laura.xchagas@gmail.com>
2024-03-21 23:09:12 -0300

    Upload CadastroCD.java file

---
+++ JavaExercises/src/main/java/CadastroCD.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CadastroCD {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nameAlbum, artist;
+        int cont;
+
+        System.out.print("Informe o nome do Álbum: ");
+        nameAlbum = sc.nextLine();
+
+        System.out.print("Informe o artista: ");
+        artist = sc.nextLine();
+
+        System.out.print("Informe a quantidade de músicas que contém no álbum: ");
+        cont = sc.nextInt();
+
+        String[] music = new String[cont];
+
+        for (int i = 0; i < music.length; i++) {
+            System.out.print("Digite a " + (i+1) +" música da banda: ");
+            music[i] = sc.next();
+        }
+
+        System.out.println();
+        System.out.println("Álbum: " + nameAlbum + "\nArtista: " + artist + "\nMúsicas: ");
+        for (int i = 0; i < music.length; i++) {
+            System.out.println((i+1) +"ª " + music[i]);
+        }
+
+    }
+}
