5a59a49409ccf52789e4d2acfc23c54610ce53cc
Chagas <laura.xchagas@gmail.com>
2024-03-21 22:40:09 -0300

    upload AreaRetangulo.java file

---
+++ JavaExercises/src/main/java/AreaRetangulo.java
@@ -0,0 +1,11 @@
+public class AreaRetangulo {
+    public static void main(String[] args) {
+
+        double alt = 2.5, base = 2.5, area;
+
+        area = alt * base;
+
+        System.out.print("A área do Retângulo é respectivamente: " + area);
+
+    }
+}
